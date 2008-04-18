6d49262490f13ab4ccf55f5bd79f074e506b3dc1
Alexander.Chukov <Alexander.Chukov@5ceb152f-8d49-0410-b65f-055112f74a87>
2008-04-18 17:42:46 +0000

    IconSet files

---
+++ IconSet.h
@@ -0,0 +1,43 @@
+#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIView.h>
+#import <UIKit/UIApplication.h>
+#import <UIKit/UIPushButton.h>
+#import <UIKit/UITableCell.h>
+#import <UIKit/UIImageAndTextTableCell.h>
+#import <UIKit/UIPreferencesTable.h>
+#import <UIKit/UIPreferencesTableCell.h>
+#import <UIKit/UIPreferencesTextTableCell.h>
+#import <UIKit/UIPreferencesControlTableCell.h>
+#import <UIKit/UIPreferencesDeleteTableCell.h>
+#import <UIKit/UISwitchControl.h>
+#import <UIKit/UIControl.h>
+
+enum {
+    ICON_JABBER = 0,
+    ICON_GTALK,
+    ICON_ICQ,
+    ICON_ONLINE,
+    ICON_AWAY,
+    ICON_XAWAY,
+    ICON_DND,
+    ICON_CHAT,
+    ICON_OFFLINE,
+    ICON_CONTENT,
+    ICON_MAX
+};
+
+@interface IconSet: NSObject {
+    UIImage *_image[ICON_MAX];
+}
+
+- (id) init;
+- (UIImage *) getIcon:(int) i;
+- (UIImage *) getIconForJID:(NSString *) jid;
+- (void) dealloc;
+
++ (id) initSharedInstance;
++ (id) sharedInstance;
+
+@end
---
+++ IconSet.m
@@ -0,0 +1,75 @@
+#import "IconSet.h"
+
+static id sharedInstanceIcon;
+
+static NSString *file[ICON_MAX] = {
+    @"jabber-small.png",
+    @"gtalk-small.png",
+    @"icq-small.png",
+    @"available.png",
+    @"away.png",
+    @"xaway.png",
+    @"dnd.png",
+    @"chat.png",
+    @"offline.png",
+    @"content.png"
+};
+
+@implementation IconSet
+
+    + (id) initSharedInstance
+    {
+	sharedInstanceIcon = [[IconSet alloc] init];
+	return sharedInstanceIcon;
+    }
+
+    + (id) sharedInstance
+    {
+	return sharedInstanceIcon;
+    }
+
+    - (id) init
+    {
+	int i;
+
+	self  = [super init];
+
+	for (i = 0; i < ICON_MAX; i++) {
+	    _image[i] = [UIImage applicationImageNamed: file[i]];
+	    if (_image[i] == nil)
+		NSLog(@"Can't load image %@", file[i]);
+	}
+	
+	return self;
+    }
+
+    - (UIImage *) getIcon:(int) i
+    {
+	return _image[i];
+    }
+
+    - (UIImage *) getIconForJID:(NSString *) jid
+    {
+	int n = ICON_JABBER;
+	NSRange range = [jid rangeOfString:@"@gmail.com" options:(NSCaseInsensitiveSearch)];
+	if (range.location != NSNotFound)
+	    n = ICON_GTALK;
+	range = [jid rangeOfString:@"icq" options:(NSCaseInsensitiveSearch)];
+	if (range.location != NSNotFound)
+	    n = ICON_ICQ;
+	
+	return _image[n];
+    }
+
+    - (void) dealloc
+    {
+	int i;
+	for (i = 0; i < 0; i++) {
+	    if (_image[i] != nil)
+		[_image[i] release];
+	}
+	
+	[super dealloc];
+    }
+
+@end
