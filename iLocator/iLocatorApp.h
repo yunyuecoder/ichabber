7af7f9783294bbe5b07d7fad89ed11e4dd423070
Alexander.Chukov <Alexander.Chukov@5ceb152f-8d49-0410-b65f-055112f74a87>
2008-06-11 15:58:56 +0000

    poslist

--- iLocator/iLocatorApp.h
+++ iLocator/iLocatorApp.h
@@ -6,18 +6,21 @@
 #import <CoreTelephony/CoreTelephony.h>
 #import <stdio.h>
 #import <string.h>
+#import <unistd.h>
 
 #import "LLData.h"
 
 @interface iLocatorApp: NSObject {
     CellInfo cellinfo;
     NSMutableArray *theDataArray;
+    int update_delay;
 }
 
 - (void) dealloc;
 - (void)addLocationFromPacket:(NSMutableArray *) packet;
 - (void) cellConnect;
 - (void) getCellInfo:(int) cell;
-- (LLData *) showLocationWithMNC:(int) MNC andMCC:(int) MCC andCID:(int) CID andLAC:(int) LAC;
+- (LLData *) showLocationWithMNC:(int) MNC andMCC:(int) MCC andCID:(int) CID andLAC:(int) LAC andRX:(int) rxl;
+- (void) setUpdateDelay:(int) delay;
 - (void) main;
 @end
